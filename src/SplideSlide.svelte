79bb97aab1ba4f74e2fe2f51fe03a430cdabdc75
Lorenzo Fiore <fiorelorenzo.fl@gmail.com>
2020-09-02 23:49:20 +0200

    Setup github pages

--- src/SplideSlide.svelte
+++
@@ -1,3 +0,0 @@
-<li class="splide__slide">
-  <slot />
-</li>
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import Splide from "./Splide.svelte";
-import SplideSlide from "./SplideSlide.svelte";
-
-export default Splide;
-
-export { Splide, SplideSlide };
